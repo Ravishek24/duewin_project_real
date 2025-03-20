57aeb65db8fea4aab666de33f08c8e8e4b4e38db
Ravishek Singh <ravishekstar@gmail.com>
2025-03-20 13:54:13 +0530

    edit db name

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-export const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+export const sequelize = new Sequelize(process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: 'mysql', // Replace with your database dialect if not MySQL
     logging: false, // Disable SQL query logs; optional
